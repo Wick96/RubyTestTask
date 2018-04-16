6bd62756f0a26d9e326344ef331c2429449046b0
Wick96 <lbasa1@gmail.com>
2018-04-16 15:05:13 +0200

    add .env to linked files

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, "https://github.com/Wick96/RubyTestTask.git"
 
 set :deploy_to, '/home/deploy/cat_cemetery'
 
-append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_files, "config/database.yml", "config/secrets.yml", ".env"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
 
 # Default branch is :master
